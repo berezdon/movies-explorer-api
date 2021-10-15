1693f38c22dc9ddb27eb61e39edabec346754aff
Maksim Spirin <berezdon@yandex.ru>
2021-10-15 17:47:50 +0300

    feat: Правки

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 
-router.use(require('./rateLimit'));
-router.use(require('./helmet'));
 router.use(require('./signin'));
 router.use(require('./signup'));
-router.use(require('./signout'));
 
 router.use(auth);
 router.use(require('./user'));
 router.use(require('./movie'));
-router.use(require('./centralizedErrorHandler'));
+router.use(require('./signout'));
 
 module.exports = router;
