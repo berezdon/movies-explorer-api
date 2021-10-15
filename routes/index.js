a46e6483254370698815ff18ee1a6ef137d016f1
Maksim Spirin <berezdon@yandex.ru>
2021-10-15 17:21:13 +0300

    feat: Правка Cors

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const auth = require('../middlewares/auth');
 router.use(require('./rateLimit'));
 router.use(require('./helmet'));
 router.use(require('./signin'));
+router.use(require('./signup'));
 router.use(require('./signout'));
 
 router.use(auth);
