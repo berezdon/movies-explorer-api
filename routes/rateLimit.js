7846a6858cb25a007871f249ce3783a3ae37e1b9
Maksim Spirin <berezdon@yandex.ru>
2021-10-17 23:31:21 +0300

    feat: Правки rateLimit

--- routes/rateLimit.js
+++ routes/rateLimit.js
@@ -4,8 +4,8 @@ const rateLimit = require('express-rate-limit');
 router.use(
   rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour duration in milliseconds
-    max: 100,
-    message: 'You exceeded 100 requests in 1 hour limit!',
+    max: 1000,
+    message: 'You exceeded 1000 requests in 1 hour limit!',
     headers: true,
   }),
 );
